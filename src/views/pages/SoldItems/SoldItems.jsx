29d3d6aefc4a3f58cc57ac0e71bf7e85dd9a4c0a
laraib <laraib@99technologies.co>
2023-11-27 15:39:36 +0500

    fixes

--- src/views/pages/SoldItems/SoldItems.jsx
+++ src/views/pages/SoldItems/SoldItems.jsx
@@ -21,20 +21,7 @@ const SoldItems = () => {
 		setBatchNumber(e.target.value);
 		setRefresh(prev => prev + 1);
 	};
-	const getColorForStatus = status => {
-		switch (status) {
-			case 'error':
-				return 'red';
-			case 'pending':
-				return '#ED6C02';
-			case 'processing':
-				return '#2196F3';
-			case 'processed':
-				return '#2E7D32';
-			default:
-				return 'black'; // or any default color
-		}
-	};
+
 	const intialColumns = [
 		{
 			accessorKey: 'file.batch_number',
@@ -70,85 +57,6 @@ const SoldItems = () => {
 		},
 	];
 
-	const uploadsoldColumns = [
-		{
-			accessorKey: 'name',
-			header: 'File Name',
-			size: 100,
-		},
-		{
-			accessorKey: 'uploaded_date_time',
-			header: 'Upload Date, Time',
-			Cell: ({ renderedCellValue }) => (
-				<>{formatDateToYYYYMMDD(renderedCellValue)}</>
-			),
-		},
-
-		{
-			accessorKey: 'status',
-			header: 'Status',
-			Cell: ({ row }) => (
-				<TableCell
-					// onClick={() => handleStatusClick(row)}
-					style={{
-						color: getColorForStatus(row.original.status),
-						border: 'none',
-						background: 'none',
-					}}
-				>
-					<Typography sx={{ textTransform: 'capitalize' }}>
-						{row.original.status}
-					</Typography>
-				</TableCell>
-			),
-		},
-		{
-			accessorKey: 'batch_number',
-			header: 'Batch No',
-			Cell: ({ row }) => (
-				<Tooltip
-					title={
-						<>
-							{`Total Ready: ${row?.original?.ready_items_count}`}
-							<br />
-							{`Total Non Ready: ${row?.original?.non_ready_items_count}`}
-							<br />
-							{`Total Sold: ${row?.original?.sold_items_count}`}
-						</>
-					}
-					sx={{ padding: '20px' }}
-				>
-					{row?.original?.batch_number}
-				</Tooltip>
-			),
-		},
-		{
-			accessorKey: 'batch_number',
-			header: 'Actions',
-			size: 300,
-			Cell: ({ row }) => (
-				<Stack direction={'row'} spacing={2}>
-					<Button
-						id={row?.original?.id}
-						variant='contained'
-						// onClick={() => {
-						// 	setFileDownLoadId(row?.original?.id);
-						// 	setDownloadModal(true);
-						// }}
-						// disabled={downloading === row?.original?.id}
-					>
-						{/* {downloading === row?.original?.id ? (
-							<Box sx={{ width: '100%' }}>
-								<LinearProgressWithLabel value={progress} />
-							</Box>
-						) : ( */}
-						<Download />
-						{/* )} */}
-					</Button>
-				</Stack>
-			),
-		},
-	];
 	return (
 		<>
 			<Grid container>
@@ -157,117 +65,23 @@ const SoldItems = () => {
 
 					<FileUploadTable
 						sx={{ my: 3 }}
-						importFileHeading='Upload Sold Items File'
+						importFileHeading='Upload Ready Items File'
 						type='sold'
 					/>
 					<TableContainer>
-						<Grid item container>
-							<>
-								<Grid item sm={6} display='flex' alignItems='center'>
-									<Stack
-										direction='row'
-										display='flex'
-										alignItems='center'
-										spacing={0}
-									>
-										<Typography
-											variant='h6'
-											component='span'
-											sx={{ padding: '24px' }}
-										>
-											Uploaded Inventory Files
-										</Typography>
-									</Stack>
-								</Grid>
-
-								<Grid
-									item
-									sm={6}
-									sx={{
-										display: 'flex',
-										justifyContent: 'end',
-										alignItems: 'center',
-									}}
-								>
-									<Box sx={{ width: '400px', marginRight: '22px' }}>
-										<FormControl fullWidth>
-											<InputLabel id='dropdown-label'>Batch No</InputLabel>
-											<Select
-												labelId='dropdown-label'
-												id='dropdown'
-												// value={selectedValue}
-												label='Select an Option'
-												// onChange={handleChange}
-												MenuProps={{
-													anchorOrigin: {
-														vertical: 'bottom',
-														horizontal: 'left',
-													},
-													transformOrigin: {
-														vertical: 'top',
-														horizontal: 'left',
-													},
-													getContentAnchorEl: null,
-													PaperProps: {
-														style: {
-															maxHeight: '500px',
-														},
-													},
-												}}
-											>
-												<MenuItem
-													onClick={() => {
-														setBatchNumber(null);
-														setRefresh(prev => prev + 1);
-													}}
-												>
-													All Files
-												</MenuItem>
-												<Divider />
-												{/* {batchList?.map(row => (
-														<MenuItem
-															key={row.id}
-															value={row?.id}
-															onClick={() => {
-																setBatchNumber(row?.batch_number);
-																setRefresh(prev => prev + 1);
-															}}
-														>
-															{row?.batch_number}
-														</MenuItem>
-													))} */}
-											</Select>
-										</FormControl>
-									</Box>
-								</Grid>
-							</>
-						</Grid>
+						<DataTableExtendedHeader
+							onSearchSubmit={handleSearchChange}
+							onBatchChange={handleBatchChange}
+							type='Sold'
+						/>
 
 						<DataTable
 							api={e => getSolditems(e, bathcNumber, searchText)}
 							columns={intialColumns}
 							onRowClick={() => {}}
 							refresh={refresh}
-							manualFilter
 						/>
 					</TableContainer>
-					<Box mt={2}>
-						<TableContainer>
-							<DataTableExtendedHeader
-								onSearchSubmit={handleSearchChange}
-								onBatchChange={handleBatchChange}
-								type='Sold'
-							/>
-							<DataTable
-								api={e => getSolditems(e, bathcNumber, searchText)}
-								columns={intialColumns}
-								setSelectedRows={setSelectedRows}
-								onRowClick={() => {}}
-								// collapsed={viewItem}
-								refresh={refresh}
-							/>
-						</TableContainer>
-					</Box>
 				</Grid>
 			</Grid>
 		</>
