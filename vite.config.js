049afff967415cd044f80c9e2d3474f39afe652a
umarzahir <umar.zahir@99technologies.co>
2023-12-04 17:10:06 +0500

    d

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			src: path.resolve('./src/'), // You can set up your own aliases here
-			comp: path.resolve('./src/views/components'),
-			pages: path.resolve('./src/views/pages'),
-			core: path.resolve('./src/core'),
-			assets: path.resolve('./src/assets'),
+			src: path.resolve(__dirname, './src/'), // You can set up your own aliases here
+			comp: path.resolve(__dirname, './src/views/components'),
+			pages: path.resolve(__dirname, './src/views/pages'),
+			core: path.resolve(__dirname, './src/core'),
+			assets: path.resolve(__dirname, './src/assets'),
 		},
 	},
 });
