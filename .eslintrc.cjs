77814b965c31b06cff25f39e233433a3b5d5c435
umarzahir <umar.zahir@99technologies.co>
2023-12-04 17:38:23 +0500

    f

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,7 @@
 module.exports = {
 	root: true,
-	globals: {
-		__dirname: true,
-	},
-	env: { browser: true, es2020: true },
+
+	env: { node: true, es2020: true },
 	extends: [
 		'eslint:recommended',
 		'plugin:react/recommended',
