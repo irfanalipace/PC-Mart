25719f507ea10f6df452bace70eb187aa5e5e768
umarzahir <umar.zahir@99technologies.co>
2023-12-04 17:31:57 +0500

    added global dirnam

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// Determine the directory of the current module
-const __dirname = path.resolve(new URL('.', import.meta.url).pathname);
-
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
+			// '@': path.resolve(__dirname, './src'),
 			src: path.resolve(__dirname, 'src'), // Adjusted the path here
 			comp: path.resolve(__dirname, 'src/views/components'),
 			pages: path.resolve(__dirname, 'src/views/pages'),
