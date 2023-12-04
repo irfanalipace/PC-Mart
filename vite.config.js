497040cc1498b00bfcc776c17841257e6abd2136
umarzahir <umar.zahir@99technologies.co>
2023-12-04 17:06:53 +0500

    idi

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			src: '/src', // You can set up your own aliases here
-			comp: '/src/views/components',
-			pages: '/src/views/pages',
-			core: '/src/core',
-			assets: '/src/assets',
+			src: path.resolve('./src/'), // You can set up your own aliases here
+			comp: path.resolve('./src/views/components'),
+			pages: path.resolve('./src/views/pages'),
+			core: path.resolve('./src/core'),
+			assets: path.resolve('./src/assets'),
 		},
 	},
 });
